b215f3377be0b9106b49a77744a55072e7eb2ce1
Cédric Tissières <cedric.tissieres@objectif-securite.ch>
2016-01-08 21:32:23 +0100

    upstream lib bumped to version 2.1.4

--- tw2/jquery/defaults.py
+++ tw2/jquery/defaults.py
@@ -1,8 +1,8 @@
-_version_ = '1.10.2'
-_dirname_ = 'jquery/%(version)s'  # This will be used to generate links
-_basename_ = 'jquery'
+_version_ = '2.1.4'
+_dirname_ = 'jquery/latest'  # This will be used to generate links
+_basename_ = 'jquery.min'
 # same, but for plugins
-_plugin_dirname_ = 'jquery/plugins/%(name)s/%(version)s/%(subdir)s'
+_plugin_dirname_ = 'jquery/plugins/%(name)s/latest/%(subdir)s'
 _plugin_subdir_ = 'js'
 _plugin_basename_ = 'jquery.%(name)s'
 
